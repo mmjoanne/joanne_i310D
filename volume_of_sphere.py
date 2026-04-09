328d41df8bef13963912cbbb67a1b01af3eeb420
Joanne Mittapalli <joannemittapalli@wireless-10-155-129-34.public.utexas.edu>
2026-04-08 22:48:07 -0500

    added sphere

---
+++ volume_of_sphere.py
@@ -0,0 +1,7 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14159
+    volume = (4/3) * pi * (radius ** 3)
+    return volume
+
+print(f"Volume of sphere with radius 30: {calculate_volume_of_sphere(30)}")
+print(f"Volume of sphere with radius 40: {calculate_volume_of_sphere(40)}")
